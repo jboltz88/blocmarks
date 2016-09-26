d8b8129846d5f67907c5819033650fd89806ab37
Justin Boltz <boltz.justin@gmail.com>
2016-09-26 13:58:11 -0500

    Add devise user model

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable,
+         :recoverable, :rememberable, :trackable, :validatable
+  before_save { self.email = email.downcase if email.present? }
+
+  validates :name, length: { minimum: 1, maximum: 100 }, presence: true
+
+end
